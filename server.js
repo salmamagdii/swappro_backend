a6dd777f4f0784565d9494d856193998d366b2fd
salmamagdii <salma.magdy@stu.eng.aswu.edu.eg>
2025-06-03 17:34:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -107,7 +107,8 @@ app.post('/signin', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
